9be86734ecf75a56fc368eb8d1e7024a6619755a
omri143 <omri143@users.noreply.github.com>
2021-05-11 13:55:54 +0300

    Client code changed

--- Testing/Exercisees/TCP/multi_clients_server/client.py
+++ Testing/Exercisees/TCP/multi_clients_server/client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 SERVER_ADDRESS = "127.0.0.1"
 SERVER_PORT = 5555
@@ -6,5 +7,5 @@ SERVER_PORT = 5555
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((SERVER_ADDRESS, SERVER_PORT))
 while True:
-    #client_socket.send(("Hi from " + client_socket.getpeername()[0] + "\n").encode())
-    pass
+    client_socket.send(("Hi from " + client_socket.getpeername()[0] + "\n").encode())
+    print(client_socket.recv(1024).decode())
