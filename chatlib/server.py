de4f1142c60ff2665630b466fff23499301fa56f
omri143 <omri143@users.noreply.github.com>
2021-05-12 19:30:54 +0300

    Added function that loads the questions from json file

--- chatlib/server.py
+++ chatlib/server.py
@@ -6,9 +6,11 @@ import socket
 import chatlib
 
 # GLOBALS
+import helpers
+
 users = {}
 questions = {}
-logged_users = {} # a dictionary of client hostnames to usernames - will be used later
+logged_users = {}  # a dictionary of client hostnames to usernames - will be used later
 
 ERROR_MSG = "Error! "
 SERVER_PORT = 5678
@@ -19,30 +21,34 @@ SERVER_IP = "127.0.0.1"
 
 def build_and_send_message(conn, code, msg):
 	## copy from client
-	
-	print("[SERVER] ",full_msg)	  # Debug print
+
+	print("[SERVER] ", full_msg)  # Debug print
+
 
 def recv_message_and_parse(conn):
 	## copy from client
-	
-	print("[CLIENT] ",full_msg)	  # Debug print
-	
+
+	print("[CLIENT] ", full_msg)  # Debug print
 
 
 # Data Loaders #
 
-def load_questions():
+def load_questions(path):
 	"""
 	Loads questions bank from file	## FILE SUPPORT TO BE ADDED LATER
 	Recieves: -
 	Returns: questions dictionary
 	"""
-	questions = {
-				2313 : {"question":"How much is 2+2","answers":["3","4","2","1"],"correct":2},
-				4122 : {"question":"What is the capital of France?","answers":["Lion","Marseille","Paris","Montpellier"],"correct":3} 
-				}
-	
+	json_dict = helpers.parse_json_doc(path)
+	for i in range(0, len(json_dict["questions"])):
+		questions[str(json_dict["questions"][i][helpers.JSON_QUESTIONS_ATTRIBUTES[0]]).zfill(4)] = {
+			"question": json_dict["questions"][i][helpers.JSON_QUESTIONS_ATTRIBUTES[1]],
+			"answers": json_dict["questions"][i][helpers.JSON_QUESTIONS_ATTRIBUTES[2]],
+			"correct": json_dict["questions"][i][helpers.JSON_QUESTIONS_ATTRIBUTES[3]]
+		}
+
 	return questions
+
 
 def load_user_database():
 	"""
@@ -51,13 +57,13 @@ def load_user_database():
 	Returns: user dictionary
 	"""
 	users = {
-			"test"		:	{"password":"test","score":0,"questions_asked":[]},
-			"yossi"		:	{"password":"123","score":50,"questions_asked":[]},
-			"master"	:	{"password":"master","score":200,"questions_asked":[]}
-			}
+		"test": {"password": "test", "score": 0, "questions_asked": []},
+		"yossi"	:	{"password" :"123", "score": 50, "questions_asked": []},
+		"master": {"password": "master", "score": 200, "questions_asked": []}
+	}
 	return users
 
-	
+
 # SOCKET CREATOR
 
 def setup_socket():
@@ -67,40 +73,41 @@ def setup_socket():
 	Returns: the socket object
 	"""
 	# Implement code ...
-	
+
 	return sock
-	
 
 
-		
 def send_error(conn, error_msg):
 	"""
 	Send error message with given message
 	Recieves: socket, message error string from called function
 	Returns: None
 	"""
-	# Implement code ...
-	
 
 
-	
+# Implement code ...
+
+
 ##### MESSAGE HANDLING
 
 
-def handle_getscore_message(conn, username):
+def handle_get_score_message(conn, username):
 	global users
-	# Implement this in later chapters
 
-	
+
+# Implement this in later chapters
+
+
 def handle_logout_message(conn):
 	"""
-	Closes the given socket (in laster chapters, also remove user from logged_users dictioary)
+	Closes the given socket (in laster chapters, also remove user from logged_users dictionary)
 	Recieves: socket
 	Returns: None
 	"""
 	global logged_users
-	
-	# Implement code ...
+
+
+# Implement code ...
 
 
 def handle_login_message(conn, data):
@@ -111,9 +118,10 @@ def handle_login_message(conn, data):
 	Returns: None (sends answer to client)
 	"""
 	global users  # This is needed to access the same users dictionary from all functions
-	global logged_users	 # To be used later
-	
-	# Implement code ...
+	global logged_users  # To be used later
+
+
+# Implement code ...
 
 
 def handle_client_message(conn, cmd, data):
@@ -122,24 +130,24 @@ def handle_client_message(conn, cmd, data):
 	Recieves: socket, message code and data
 	Returns: None
 	"""
-	global logged_users	 # To be used later
-	
-	# Implement code ...
-	
+	global logged_users  # To be used later
+
+
+# Implement code ...
 
 
 def main():
-	# Initializes global users and questions dicionaries using load functions, will be used later
+	# Initializes global users and questions dictionaries using load functions, will be used later
 	global users
 	global questions
-	
-	print("Welcome to Trivia Server!")
-	
-	# Implement code ...
 
+	print("Welcome to Trivia Server!")
+	load_questions("C:\\Users\\OMRI\\PycharmProjects\\networksPy\\chatlib\\questions.json")
+	print(questions)
+
+
+# Implement code ...
 
 
 if __name__ == '__main__':
 	main()
-
-	
