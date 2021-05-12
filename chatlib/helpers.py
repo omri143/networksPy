b493b804862be26829d78c59749b3dc9019dd2e2
omri143 <omri143@users.noreply.github.com>
2021-05-12 14:33:18 +0300

    Added Json support

--- chatlib/helpers.py
+++ chatlib/helpers.py
@@ -1,3 +1,9 @@
+import json
+
+### COMMON METHODS ####
+import tarfile
+
+
 def get_digits_count(num):
     """
     Helper method. Finds the amount of digits inside a number
@@ -12,6 +18,15 @@ def get_digits_count(num):
 
 
 def format_list_print(recv_data_list):
-
     for i in range(2, len(recv_data_list)):
-        print(str(i-1)+". " + recv_data_list[i])
+        print(str(i - 1) + ". " + recv_data_list[i])
+
+
+### Json ####
+def parse_json_doc(path):
+    json_data_str = None
+    with open(path, 'r') as f:
+        json_data_str = f.readlines()
+        f.close()
+    json_data = json.loads("".join(json_data_str))
+    return json_data
